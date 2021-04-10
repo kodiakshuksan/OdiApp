6dbbc976e985713ca4b1a46fc02f1c875736bc2e
kodiakshuksan <74173891+kodiakshuksan@users.noreply.github.com>
2021-04-09 22:33:23 -0700

    added practice function to test before adding real equation

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ console.log('I love Wild Dog!')
 function myFunction() {
     const humanYears = document.getElementById("humanYears").value;
     console.log(humanYears)
+    const dogAge = humanYears * 10;
   
-    document.getElementById("dogsRealAge").innerHTML = humanYears;
+    document.getElementById("dogsRealAge").innerHTML = dogAge;
   }
