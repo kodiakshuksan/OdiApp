2c9a8ac314370f6ef1d12633d19f365d05ce9c1b
kodiakshuksan <74173891+kodiakshuksan@users.noreply.github.com>
2021-04-10 00:55:20 -0700

    added css and image

--- script.js
+++ script.js
@@ -3,24 +3,6 @@
 console.log('I love Wild Dog!')
 
 
-// // your form
-// const x = document.getElementById("humanYears").value;
-// console.log(x)
-
-// const dogAge = function(event) {
-// humanYears * 10 
-//     console.log(humanYears)
-//     event.preventDefault(); 
-    
-//     document.getElementById("dogsRealAge").innerHTML = dogAge;
-    
-// };
-
-// const newDogAge = dogAge;
-
-// attach event listener
-// humanYears.addEventListener("submit", dogAge);
-
 function myFunction() {
     const humanYears = document.getElementById("humanYears").value;
     console.log(humanYears)
