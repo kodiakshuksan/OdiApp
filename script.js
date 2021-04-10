c81fd87ea0bc8f65874ab56f76d84f33c27af4cc
kodiakshuksan <74173891+kodiakshuksan@users.noreply.github.com>
2021-04-09 22:29:03 -0700

    added button, added function

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 // JavaScript for Odi App
+// making sure javascript is connecting
 console.log('I love Wild Dog!')
 
-const x = userInput;
 
+// // your form
+// const x = document.getElementById("humanYears").value;
+// console.log(x)
+
+// const dogAge = function(event) {
+// humanYears * 10 
+//     console.log(humanYears)
+//     event.preventDefault(); 
+    
+//     document.getElementById("dogsRealAge").innerHTML = dogAge;
+    
+// };
+
+// const newDogAge = dogAge;
+
+// attach event listener
+// humanYears.addEventListener("submit", dogAge);
+
+function myFunction() {
+    const humanYears = document.getElementById("humanYears").value;
+    console.log(humanYears)
+  
+    document.getElementById("dogsRealAge").innerHTML = humanYears;
+  }
